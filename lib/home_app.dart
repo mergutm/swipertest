65e3b7bb8ebbf72c56cd8b2e4207484b16beb3b7
mergutm <merg@gs.utm.mx>
2025-07-12 16:13:39 +0000

    swiper con imagenes de pokemones

--- lib/home_app.dart
+++ lib/home_app.dart
@@ -1,39 +1,47 @@
-  import 'package:card_swiper/card_swiper.dart';
-  import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/material.dart';
 
-  class HomeApp extends StatelessWidget {
-    // Add the key parameter to the constructor
-    const HomeApp({super.key});
+class HomeApp extends StatelessWidget {
+  // Add the key parameter to the constructor
+  const HomeApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Home App')),
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.5,
-              color: Colors.amber,
-              child: Swiper(
-                itemCount: 10,
-                autoplay: true,
-                itemBuilder: (context, index) {
-                    return FadeInImage(placeholder: AssetImage("assets/notfound.jpg"), 
-                    image: NetworkImage(        
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home App')),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            color: Colors.blueGrey,
+            child: Swiper(
+              itemCount: 10,
+              itemWidth: 200,
+              layout: SwiperLayout.STACK,
+              autoplay: true,
+              itemBuilder: (context, index) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: FadeInImage(
+                    placeholder: AssetImage("assets/notfound.jpg"),
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
                       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index + 1}.png',
-));
-                },
-              ),
+                    ),
+                  ),
+                );
+              },
             ),
-            SizedBox(height: 25),
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.25,
-              color: Colors.red,
-            ),
-          ],
-        ),
-      );
-    }
+          ),
+          SizedBox(height: 25),
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.25,
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
   }
+}
