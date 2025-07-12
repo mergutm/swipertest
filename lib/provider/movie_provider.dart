abb897cc2bc653b818e9dc529b05aed27279856a
mergutm <merg@gs.utm.mx>
2025-07-12 16:23:30 +0000

    provider

---
+++ lib/provider/movie_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MovieProvider extends ChangeNotifier {
+  MovieProvider() {}
+
+  void notificarCambios() {
+    notifyListeners();
+  }
+
+  List<int> numeros = [];
+  List<String> pokes = [
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png',
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png',
+  ];
+}
