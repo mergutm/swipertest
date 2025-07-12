02b5b5eea441a56ca7d5806fcaf10e7ef0f5a02e
mergutm <merg@gs.utm.mx>
2025-07-12 16:09:08 +0000

    swiper con imagenes de pokemones

--- lib/home_app.dart
+++ lib/home_app.dart
@@ -1,28 +1,39 @@
-import 'package:flutter/material.dart';
+  import 'package:card_swiper/card_swiper.dart';
+  import 'package:flutter/material.dart';
 
-class HomeApp extends StatelessWidget {
-  // Add the key parameter to the constructor
-  const HomeApp({super.key});
+  class HomeApp extends StatelessWidget {
+    // Add the key parameter to the constructor
+    const HomeApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home App')),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.25,
-            color: Colors.amber,
-          ),
-          SizedBox(height: 25),
-          Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.25,
-            color: Colors.red,
-          ),
-        ],
-      ),
-    );
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Home App')),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.5,
+              color: Colors.amber,
+              child: Swiper(
+                itemCount: 10,
+                autoplay: true,
+                itemBuilder: (context, index) {
+                    return FadeInImage(placeholder: AssetImage("assets/notfound.jpg"), 
+                    image: NetworkImage(        
+                      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index + 1}.png',
+));
+                },
+              ),
+            ),
+            SizedBox(height: 25),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.25,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      );
+    }
   }
-}
